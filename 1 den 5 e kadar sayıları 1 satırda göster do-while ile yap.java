c987339292c3375db74bd1a5eb0ffdca67d478d8
Sheyyma <144834962+Sheyyma@users.noreply.github.com>
2023-11-26 08:52:08 +0300

    Add files via upload

---
+++ 1 den 5 e kadar sayıları 1 satırda göster do-while ile yap.java
@@ -0,0 +1,11 @@
+public class h17 {
+    //1 den 5 e kadar sayıları 1 satırda göster do-while ile yap
+    public static void main(String[] args) {
+        int i=1;
+        int sayi;
+        do {
+            System.out.print(i+ " ");
+            i++;
+        }while(i<=5);
+        }
+    }
---
+++ sayı değiştirme temp ile.java
@@ -0,0 +1,12 @@
+public class h6 {
+    // iki sayıyı değiştirme
+    public static void main(String[] args) {
+        int x=5; int y=10; int z;
+        int temp=x;
+        x=y;
+        y=temp;
+        System.out.println(x + " "+y);
+
+    }
+
+}
